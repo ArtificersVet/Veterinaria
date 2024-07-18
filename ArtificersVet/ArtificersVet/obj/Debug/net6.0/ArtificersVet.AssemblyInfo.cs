b994ff70052a31548ea5227694679e4c815785ac
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-07-17 22:12:07 -0600

    diseño consultas

--- ArtificersVet/ArtificersVet/obj/Debug/net6.0/ArtificersVet.AssemblyInfo.cs
+++ ArtificersVet/ArtificersVet/obj/Debug/net6.0/ArtificersVet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArtificersVet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+155cc3d8719ecb819cbbe66139a8194813b04cd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b64116908e623593a5d2314af0dc1d4e40ac59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArtificersVet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArtificersVet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
