5e163296ec5a8e44f8cea1b9cd0347c2638cb0bf
Emerson1911 <1284662@clases.edu.sv>
2024-07-18 20:52:48 -0600

    Documentacion de empleados terminada

--- ArtificersVet/ArtificersVet/Models/Empleado.cs
+++ ArtificersVet/ArtificersVet/Models/Empleado.cs
@@ -5,6 +5,9 @@ namespace ArtificersVet.Models
 {
     public partial class Empleado
     {
+        /// <summary>
+        /// Constructor que inicializa las colecciones para evitar problemas de referencia nula
+        /// </summary>
         public Empleado()
         {
             Consulta = new HashSet<Consulta>();
@@ -12,8 +15,14 @@ namespace ArtificersVet.Models
             HistorialMedicos = new HashSet<HistorialMedico>();
             Logins = new HashSet<Login>();
         }
-
+        /// <summary>
+        ///  // Propiedad clave primaria.
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// // Propiedades opcionales para los detalles del empleado.
+        /// Nombre,Apellido,Email,Telefono,Direccion,Puesto,Salario,FechaContratacion
+        /// </summary>
         public string? Nombre { get; set; }
         public string? Apellido { get; set; }
         public string? Email { get; set; }
@@ -22,7 +31,13 @@ namespace ArtificersVet.Models
         public string? Puesto { get; set; }
         public decimal? Salario { get; set; }
         public DateTime? FechaContratacion { get; set; }
-
+        /// <summary>
+        /// Propiedades de navegación que representan las relaciones.
+        /// Relación uno-a-muchos con Consulta
+        /// Relación uno-a-muchos con HistorialMedico1
+        /// Relación uno-a-muchos con HistorialMedico
+        /// Relación uno-a-muchos con Login
+        /// </summary>
         public virtual ICollection<Consulta> Consulta { get; set; }
         public virtual ICollection<HistorialMedico1> HistorialMedico1s { get; set; }
         public virtual ICollection<HistorialMedico> HistorialMedicos { get; set; }
